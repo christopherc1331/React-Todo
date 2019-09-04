8a7bd7098375d7f0838bd690f9a8e67a6ad6fb9e
Chris Cruz <christopherc1331@gmail.com>
2019-09-03 22:30:43 -0700

    Finished skeleton of project

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Todo = props => {
+  return <h4>{props.task.task}</h4>;
+};
+
+export default Todo;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: ""
+    };
+  }
+  render() {
+    return (
+      <div>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default TodoForm;
